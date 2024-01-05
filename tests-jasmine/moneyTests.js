fca6f68c4575df5104fa6c9b324a1690133949a0
Gloal <alori.glo@gmail.com>
2024-01-05 16:16:55 +0000

    add formatCurrency tests

---
+++ tests-jasmine/moneyTests.js
@@ -0,0 +1,20 @@
+import { formatCurrency } from '../scripts/utils/money.js'
+
+describe('test suite: formatCurrency', function() {
+    it("converts cents into dollars", function() {
+        expect(formatCurrency(2095)).toEqual("20.95");
+    });
+
+    it("works with dollar amount of zero", function() {
+        expect(formatCurrency(0)).toEqual("0.00");
+    })
+
+    it("rounds up to the nearest cent", function() {
+        expect(formatCurrency(2000.5)).toEqual("20.01");
+    })
+    
+    it("rounds down to the nearest cent", function() {
+        expect(formatCurrency(2000.4)).toEqual("20.00");
+    })
+
+});
